17a5f1fe954164545ff786b7fcd123630580a467
waffleliew <128667537+waffleliew@users.noreply.github.com>
2024-01-29 04:39:42 +0800

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'letsmakantogether',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || 'YOUR_PASSWORD',
+    process.env.DB_NAME,
+    process.env.DB_USER ,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
+        host: process.env.DB_HOST ,
+        port: process.env.DB_PORT ,
         dialect: 'mysql',
         dialectOptions: {
             ssl: {
