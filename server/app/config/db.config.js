a64e6f7626837c0615afc10f5eeb634975513fff
waffleliew <128667537+waffleliew@users.noreply.github.com>
2024-01-29 02:22:58 +0800

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,18 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('letsmakantogether', 'root', 'YOUR_PASSWORD', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
-    define: {
-        freezeTableName: true,
-        timestamps: false
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'letsmakantogether',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || 'YOUR_PASSWORD',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+        define: {
+            freezeTableName: true,
+            timestamps: false
+        }
     }
-});
+);
 
 export default sequelize;
